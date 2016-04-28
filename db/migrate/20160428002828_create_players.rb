d3efdfbada6b9f4463c20c7255c9b5f0088a4f97
Kayla Cohn <kayla.s.cohn@gmail.com>
2016-04-27 20:29:14 -0400

    Update gemfile and add Player model

---
+++ db/migrate/20160428002828_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.string :position
+      t.string :school
+      t.text :bio
+      t.string :highlight
+
+      t.timestamps null: false
+    end
+  end
+end

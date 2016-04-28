dc8965e543fa1e1f474eee3b6e70fe60d9771b90
Kayla Cohn <kayla.s.cohn@gmail.com>
2016-04-27 20:35:31 -0400

    Generate players controller and add routes

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,32 @@
+class PlayersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_player
+  end
+
+  def player_params
+  end
+  
+end

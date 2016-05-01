3a6a3e902e3a3bf268072910f1132acedf2ffacf
Kayla Cohn <kayla.s.cohn@gmail.com>
2016-05-01 13:24:50 -0400

    Delete mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,18 +89,20 @@ Rails.application.configure do
     }
   }
 
-  #Email settings
-  config.action_mailer.default_url_options = { host: 'limitless-beach-84400.herokuapp.com' }
+  # #Email settings
+  # config.action_mailer.default_url_options = { host: 'limitless-beach-84400.herokuapp.com' }
+  #
+  # config.action_mailer.delivery_method = :smtp
+  #
+  # config.action_mailer.smtp_settings = {
+  # address: "smtp.gmail.com",
+  # port: 587,
+  # domain: ENV["GMAIL_DOMAIN"],
+  # authentication: "plain",
+  # enable_starttls_auto: true,
+  # user_name: ENV["GMAIL_USERNAME"],
+  # password: ENV["GMAIL_PASSWORD"]
+  # }
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-  }
+  config.action_mailer.perform_deliveries = false
 end
